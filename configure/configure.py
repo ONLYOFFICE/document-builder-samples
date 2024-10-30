a3a651bfacbde8210dec82e3ecaf50a3bca3622d
Mikhail Lobotskiy <Mikhail.Lobotskiy@onlyoffice.com>
2024-10-30 15:16:57 +0400

    Refactoring

--- configure/configure.py
+++ configure/configure.py
@@ -50,15 +50,15 @@ class PrintTestsList(argparse.Action):
 def getSelectedTests(tests):
     all_tests = getAllTests()
     # make set of all available tests
-    tests_set = {'all'}
+    available_tests = {'all'}
     for lang in langs:
-        tests_set.add(lang)
-        tests_set.update(all_tests[lang])
+        available_tests.add(lang)
+        available_tests.update(all_tests[lang])
     # make dict with set of selected tests
     tests_selected = {lang: set() for lang in langs}
     # filter tests through only available ones
     for test in tests:
-        if not test in tests_set:
+        if not test in available_tests:
             log('warning', 'wrong test "' + test + '". Call script with --list (or -l) to see all available tests')
             continue
 
@@ -91,15 +91,19 @@ def replacePlaceholders(template_file, output_file, replacements):
         file.write(content)
 
 def genVSProjectsCPP(tests, builder_dir):
+    if os_name != 'windows':
+        log('warning', 'generating Visual Studio C++ projects is only available on Windows')
+        return
+
+    builder_dir = builder_dir.replace('/', '\\')
     for test in tests:
         test_dir = 'out/' + test
         mkdir(test_dir)
         test_name = test.split('/')[1]
         if os.path.exists(test_dir + '/' + test_name + '.vcxproj'):
-            log('info', 'VS C++ project for sample "' + test + '" already exists. Skipping.')
             continue
-        else:
-            log('info', 'generating VS C++ project for sample "' + test + '"...')
+
+        log('info', 'generating VS C++ project for sample "' + test + '"...')
         # .vcxproj
         project_guid = str(uuid.uuid4())
         replacements = {
@@ -132,15 +136,19 @@ def genVSProjectsCPP(tests, builder_dir):
         replacePlaceholders('configure/project_templates/cpp/template.vcxproj.user', test_dir + '/' + test_name + '.vcxproj.user', replacements)
 
 def genVSProjectsCS(tests, builder_dir):
+    if os_name != 'windows':
+        log('warning', 'generating Visual Studio C# projects is only available on Windows')
+        return
+
+    builder_dir = builder_dir.replace('/', '\\')
     for test in tests:
         test_dir = 'out/' + test
         mkdir(test_dir)
         test_name = test.split('/')[1]
         if os.path.exists(test_dir + '/' + test_name + '.csproj'):
-            log('info', 'VS C# project for sample "' + test + '" already exists. Skipping.')
             continue
-        else:
-            log('info', 'generating VS C# project for sample "' + test + '"...')
+
+        log('info', 'generating VS C# project for sample "' + test + '"...')
         # .csproj
         project_guid = str(uuid.uuid4())
         replacements = {
@@ -157,40 +165,20 @@ def genVSProjectsCS(tests, builder_dir):
         }
         replacePlaceholders('configure/project_templates/cs/template.sln', test_dir + '/' + test_name + '.sln', replacements)
 
-def genVSProjects(tests_selected, builder_dir):
-    if os_name != 'windows':
-        log('warning', 'generating Visual Studio projects is only available on Windows')
-        return
-
-    builder_dir = builder_dir.replace('/', '\\')
-    for lang, tests in tests_selected.items():
-        mkdir('out/' + lang)
-        if lang == 'cpp':
-            genVSProjectsCPP(tests, builder_dir)
-        elif lang == 'cs':
-            genVSProjectsCS(tests, builder_dir)
-
-def genQtProjects(tests_selected, builder_dir):
-    # only for C++ projects
-    if 'cpp' not in tests_selected:
-        return
-
+def genQtProjectsCPP(tests, builder_dir):
     root_dir = os.getcwd()
     if os_name == 'windows':
         builder_dir = builder_dir.replace('\\', '/')
         root_dir = root_dir.replace('\\', '/')
 
-    tests = tests_selected['cpp']
-    mkdir('out/cpp')
     for test in tests:
         test_dir = 'out/' + test
         mkdir(test_dir)
         test_name = test.split('/')[1]
         if os.path.exists(test_dir + '/' + test_name + '.pro'):
-            log('info', 'Qt project for sample "' + test + '" already exists. Skipping.')
             continue
-        else:
-            log('info', 'generating Qt C++ project for sample "' + test + '"...')
+
+        log('info', 'generating Qt C++ project for sample "' + test + '"...')
         # .pro
         replacements = {
             '[TEST_NAME]': test_name,
@@ -199,11 +187,7 @@ def genQtProjects(tests_selected, builder_dir):
         }
         replacePlaceholders('configure/project_templates/cpp/template.pro', test_dir + '/' + test_name + '.pro', replacements)
 
-def genMakefile(tests_selected, builder_dir):
-    # only for C++ projects
-    if 'cpp' not in tests_selected:
-        return
-
+def genMakefileCPP(tests, builder_dir):
     if os_name == 'windows':
         log('warning', 'generating Makefile is not available on Windows')
         return
@@ -220,17 +204,14 @@ def genMakefile(tests_selected, builder_dir):
         compiler = 'clang++'
         env_lib_path = 'DYLD_LIBRARY_PATH'
     root_dir = os.getcwd()
-    tests = tests_selected['cpp']
-    mkdir('out/cpp')
     for test in tests:
         test_dir = 'out/' + test
         mkdir(test_dir)
         test_name = test.split('/')[1]
         if os.path.exists(test_dir + '/Makefile'):
-            log('info', 'Makefile for sample "' + test + '" already exists. Skipping.')
-        else:
-            log('info', 'generating Makefile for C++ sample "' + test + '"...')
             continue
+
+        log('info', 'generating Makefile for C++ sample "' + test + '"...')
         # Makefile
         replacements = {
             '[TEST_NAME]': test_name,
@@ -241,6 +222,26 @@ def genMakefile(tests_selected, builder_dir):
             '[ENV_LIB_PATH]': env_lib_path
         }
         replacePlaceholders('configure/project_templates/cpp/Makefile', test_dir + '/Makefile', replacements)
+
+def genCPP(projects, tests, builder_dir):
+    mkdir('out/cpp')
+    # VS
+    if projects['vs']:
+        genVSProjectsCPP(tests, builder_dir)
+    # Qt
+    if projects['qt']:
+        genQtProjectsCPP(tests, builder_dir)
+    # Makefile
+    if projects['make']:
+        genMakefileCPP(tests, builder_dir)
+
+def genCS(projects, tests, builder_dir):
+    mkdir('out/cs')
+    # VS
+    if projects['vs']:
+        genVSProjectsCS(tests, builder_dir)
+    else:
+        log('warning', 'generating C# projects only available ' + ('on Windows ' if os_name != 'windows' else '') + 'with --vs')
 
 
 if __name__ == '__main__':
@@ -255,9 +256,9 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--test', dest='tests', action='append', help='specifies tests to generate project files', required=True)
     parser.add_argument('-l', '--list', action=PrintTestsList, nargs=0, help='show list of available tests and exit')
 
-    builder_dir = getDefaultBuilderDir()
-    if builder_dir:
-        parser.add_argument('--dir', action='store', help='specifies Document Builder directory (default: ' + builder_dir + ')', default=builder_dir)
+    default_builder_dir = getDefaultBuilderDir()
+    if default_builder_dir:
+        parser.add_argument('--dir', action='store', help='specifies Document Builder directory (default: ' + default_builder_dir + ')', default=default_builder_dir)
     else:
         parser.add_argument('--dir', action='store', help='specifies Document Builder directory', required=True)
 
@@ -275,19 +276,11 @@ if __name__ == '__main__':
         if os_name != 'windows':
             args.make = True
 
+    projects = {'vs': args.vs, 'qt': args.qt, 'make': args.make}
     # filter tests
     tests_selected = getSelectedTests(args.tests)
-
-    # generate projects
+    # generate project files
     mkdir('out')
-    # VS
-    if args.vs:
-        genVSProjects(tests_selected, args.dir)
-    elif 'cs' in tests_selected:
-        log('warning', 'generating C# projects only available ' + ('on Windows ' if os_name != 'windows' else '') + 'with --vs')
-    # Qt
-    if args.qt:
-        genQtProjects(tests_selected, args.dir)
-    # Makefile
-    if args.make:
-        genMakefile(tests_selected, args.dir)
+    handlers = {'cpp': genCPP, 'cs': genCS}
+    for lang, tests in tests_selected.items():
+        handlers[lang](projects, tests, args.dir)
